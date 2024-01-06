db941012acd86feeb777f1da072f88da0ea99b0b
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-06 18:44:51 +0800

    :zap: api updates

--- api/scrapeNews.js
+++ api/scrapeNews.js
@@ -1,7 +1,13 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 
 async function scrapeNews(url) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
   const page = await browser.newPage();
   await page.goto(url);
 
