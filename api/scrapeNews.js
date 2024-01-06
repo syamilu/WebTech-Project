7e4276ce87f3845e75f15d6cba792705f2d01bd3
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-07 00:30:34 +0800

    :zap: test

--- api/scrapeNews.js
+++ api/scrapeNews.js
@@ -1,23 +1,31 @@
-const fetch = require("node-fetch");
-const { parse } = require("node-html-parser");
+const puppeteer = require("puppeteer");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
 
 async function scrapeNews(url) {
-  const res = await fetch(url);
-  const html = await res.text();
-  const root = parse(html);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
 
-  const allNews = root
-    .querySelectorAll("article")
-    .slice(0, 9)
-    .map((newItem) => {
-      const title = newItem
-        .querySelector(".entry-header a:not(.entry-meta a)")
-        .getAttribute("title");
-      const description = newItem.querySelector(".entry-content p").innerText;
-      const link = newItem.querySelector("a").getAttribute("href");
-      const img = newItem.querySelector("img").getAttribute("src");
-      return { title, description, link, img };
-    });
+  const allNews = await page.evaluate(() => {
+    const news = document.querySelectorAll("article");
+
+    return Array.from(news)
+      .slice(0, 9)
+      .map((newItem) => {
+        const title = newItem.querySelector(
+          ".entry-header a:not(.entry-meta a)"
+        ).title;
+        const description = newItem.querySelector(".entry-content p").innerText;
+        const link = newItem.querySelector("a").href;
+        const img = newItem.querySelector("img").src;
+        return { title, description, link, img };
+      });
+  });
 
   return allNews;
 }
@@ -26,3 +34,12 @@ module.exports = async (req, res) => {
   const news = await scrapeNews("https://news.iium.edu.my/?cat=4");
   res.json(news);
 };
+
+// app.get("/api", async (req, res) => {
+//   const news = await scrapeNews("https://news.iium.edu.my/?cat=4");
+//   res.json(news);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
