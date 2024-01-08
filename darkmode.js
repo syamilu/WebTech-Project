7195d33594e4fb18c99e665a6806195f3d18b46c
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-08 17:56:21 +0800

    :zap: dark  mode for website

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,28 @@
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
+  document.getElementById("darkmode-toggle").checked = true;
+  document
+    .querySelectorAll(".topnav, .topnav a.active, #report-form")
+    .forEach(function (el) {
+      el.classList.add("dark-mode");
+    });
+}
+
 document
   .getElementById("darkmode-toggle")
   .addEventListener("change", function () {
     document.body.classList.toggle("dark-mode", this.checked);
+    document
+      .querySelectorAll(".topnav, .topnav a.active, #report-form")
+      .forEach(
+        function (el) {
+          el.classList.toggle("dark-mode", this.checked);
+        }.bind(this)
+      );
+
+    if (this.checked) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.removeItem("darkMode");
+    }
   });
