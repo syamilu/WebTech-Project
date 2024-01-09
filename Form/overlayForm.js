7b96c53ed858e63014118a966f7433311007f4d7
FynnQuinn <fiqyus09@gmail.com>
2024-01-09 16:38:23 +0800

    :zap: Added maintenace report forms

---
+++ Form/overlayForm.js
@@ -0,0 +1,19 @@
+let fetchedHTML = null;
+
+fetch("Form/forms.html")
+  .then((response) => response.text())
+  .then((data) => {
+    fetchedHTML = data;
+  });
+
+function overlayForm() {
+  if (fetchedHTML === null) {
+    setTimeout(overlayForm, 100);
+    return;
+  } else {
+    document.getElementById("form-container").innerHTML = fetchedHTML;
+  }
+  var overlay = document.getElementById("form-container");
+  overlay.style.display = overlay.style.display === "block" ? "none" : "block";
+  overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+}
