bc8b41b768ae82340421d74691fbc0891b5475f6
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-10 01:44:37 +0800

    optimizeing

--- api/getChat.js
+++ api/getChat.js
@@ -1,18 +1,48 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const getChatDataFromMongoDB = require("../../db/getChatDataFromMongoDB");
-// app.use(cors());
+// // const express = require("express");
+// // const app = express();
+// // const cors = require("cors");
+// // const getChatDataFromMongoDB = require("../../db/getChatDataFromMongoDB");
+// // app.use(cors());
 
-// app.get("/getChat", async (req, res) => {
+// // app.get("/getChat", async (req, res) => {
+// //   const chatData = await getChatDataFromMongoDB();
+// //   res.json(chatData);
+// // });
+
+// // app.listen(3000, () => console.log("Server running on port 3000"));
+
+// // api/getChat.js
+// const getChatDataFromMongoDB = require("../db/getChatDataFromMongoDB");
+
+// module.exports = async (req, res) => {
 //   const chatData = await getChatDataFromMongoDB();
 //   res.json(chatData);
-// });
+// };
 
-// app.listen(3000, () => console.log("Server running on port 3000"));
+const { MongoClient } = require("mongodb");
 
-// api/getChat.js
-const getChatDataFromMongoDB = require("../db/getChatDataFromMongoDB");
+async function getChat() {
+  const url =
+    "mongodb+srv://syamilu:asdfasdf@webtech01.yxq2azw.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const database = client.db("webtech01");
+    const collection = database.collection("messages");
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const chatData = await collection
+      .find({ timestamp: { $gte: today } })
+      .toArray();
+
+    return chatData;
+  } finally {
+    await client.close();
+  }
+}
 
 module.exports = async (req, res) => {
   const chatData = await getChatDataFromMongoDB();
