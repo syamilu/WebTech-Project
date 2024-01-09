1f2424be0211e42a8e6761ae691ec55e0d801556
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-10 02:31:44 +0800

    asdsdas

--- api/getChat.js
+++ api/getChat.js
@@ -1,27 +1,7 @@
-// // const express = require("express");
-// // const app = express();
-// // const cors = require("cors");
-// // const getChatDataFromMongoDB = require("../../db/getChatDataFromMongoDB");
-// // app.use(cors());
-
-// // app.get("/getChat", async (req, res) => {
-// //   const chatData = await getChatDataFromMongoDB();
-// //   res.json(chatData);
-// // });
-
-// // app.listen(3000, () => console.log("Server running on port 3000"));
-
-// // api/getChat.js
-// const getChatDataFromMongoDB = require("../db/getChatDataFromMongoDB");
-
-// module.exports = async (req, res) => {
-//   const chatData = await getChatDataFromMongoDB();
-//   res.json(chatData);
-// };
-
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URI;
 const client = new MongoClient(url);
 
 let collection;
@@ -58,3 +38,24 @@ module.exports = async (req, res) => {
       .json({ error: "An error occurred while fetching chat data" });
   }
 };
+
+// // const express = require("express");
+// // const app = express();
+// // const cors = require("cors");
+// // const getChatDataFromMongoDB = require("../../db/getChatDataFromMongoDB");
+// // app.use(cors());
+
+// // app.get("/getChat", async (req, res) => {
+// //   const chatData = await getChatDataFromMongoDB();
+// //   res.json(chatData);
+// // });
+
+// // app.listen(3000, () => console.log("Server running on port 3000"));
+
+// // api/getChat.js
+// const getChatDataFromMongoDB = require("../db/getChatDataFromMongoDB");
+
+// module.exports = async (req, res) => {
+//   const chatData = await getChatDataFromMongoDB();
+//   res.json(chatData);
+// };
