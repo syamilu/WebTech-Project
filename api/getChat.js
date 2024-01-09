41821a012bf93fecc4435cd9c20544d198d3cb21
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-10 01:52:52 +0800

    blabbla

--- api/getChat.js
+++ api/getChat.js
@@ -1,35 +1,17 @@
-// // const express = require("express");
-// // const app = express();
-// // const cors = require("cors");
-// // const getChatDataFromMongoDB = require("../../db/getChatDataFromMongoDB");
-// // app.use(cors());
-
-// // app.get("/getChat", async (req, res) => {
-// //   const chatData = await getChatDataFromMongoDB();
-// //   res.json(chatData);
-// // });
-
-// // app.listen(3000, () => console.log("Server running on port 3000"));
-
-// // api/getChat.js
-// const getChatDataFromMongoDB = require("../db/getChatDataFromMongoDB");
-
-// module.exports = async (req, res) => {
-//   const chatData = await getChatDataFromMongoDB();
-//   res.json(chatData);
-// };
-
 const { MongoClient } = require("mongodb");
 
-async function getChat() {
-  const url =
-    "mongodb+srv://syamilu:asdfasdf@webtech01.yxq2azw.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(url);
+const url = process.env.MONGODB_URL;
+const client = new MongoClient(url);
 
+let collection;
+
+async function getChat() {
   try {
-    await client.connect();
-    const database = client.db("webtech01");
-    const collection = database.collection("messages");
+    if (!collection) {
+      await client.connect();
+      const database = client.db("webtech01");
+      collection = database.collection("messages");
+    }
 
     const today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -39,12 +21,19 @@ async function getChat() {
       .toArray();
 
     return chatData;
-  } finally {
-    await client.close();
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 }
 
 module.exports = async (req, res) => {
-  const chatData = await getChatDataFromMongoDB();
-  res.json(chatData);
+  try {
+    const chatData = await getChat();
+    res.json(chatData);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching chat data" });
+  }
 };
