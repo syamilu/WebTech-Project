e40aa95eb5b7d03c1b2552e49ead113901e25da2
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-10 01:57:18 +0800

    getCHat problem

--- api/getChat.js
+++ api/getChat.js
@@ -1,17 +1,35 @@
+// // const express = require("express");
+// // const app = express();
+// // const cors = require("cors");
+// // const getChatDataFromMongoDB = require("../../db/getChatDataFromMongoDB");
+// // app.use(cors());
+
+// // app.get("/getChat", async (req, res) => {
+// //   const chatData = await getChatDataFromMongoDB();
+// //   res.json(chatData);
+// // });
+
+// // app.listen(3000, () => console.log("Server running on port 3000"));
+
+// // api/getChat.js
+// const getChatDataFromMongoDB = require("../db/getChatDataFromMongoDB");
+
+// module.exports = async (req, res) => {
+//   const chatData = await getChatDataFromMongoDB();
+//   res.json(chatData);
+// };
+
 const { MongoClient } = require("mongodb");
 
-const url = process.env.MONGODB_URL;
-const client = new MongoClient(url);
-
-let collection;
-
 async function getChat() {
+  const url =
+    "mongodb+srv://syamilu:asdfasdf@webtech01.yxq2azw.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(url);
+
   try {
-    if (!collection) {
-      await client.connect();
-      const database = client.db("webtech01");
-      collection = database.collection("messages");
-    }
+    await client.connect();
+    const database = client.db("webtech01");
+    const collection = database.collection("messages");
 
     const today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -21,19 +39,12 @@ async function getChat() {
       .toArray();
 
     return chatData;
-  } catch (err) {
-    console.error(err);
-    throw err;
+  } finally {
+    await client.close();
   }
 }
 
 module.exports = async (req, res) => {
-  try {
-    const chatData = await getChat();
-    res.json(chatData);
-  } catch (err) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while fetching chat data" });
-  }
+  const chatData = await getChat();
+  res.json(chatData);
 };
