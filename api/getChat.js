78aa63350b2c9b1250f5a37b5a2c69aac66ddbd5
syamilu <wanmuhammadsyamil@gmail.com>
2024-01-10 12:43:59 +0800

    :zap: hehey

--- api/getChat.js
+++ api/getChat.js
@@ -31,35 +31,3 @@ module.exports = async (req, res) => {
       .json({ error: "An error occurred while fetching chat data" });
   }
 };
-
-// module.exports = async (req, res) => {
-//   try {
-//     const chatData = await getChat();
-//     res.json(chatData);
-//   } catch (err) {
-//     res
-//       .status(500)
-//       .json({ error: "An error occurred while fetching chat data" });
-//   }
-// };
-
-// // const express = require("express");
-// // const app = express();
-// // const cors = require("cors");
-// // const getChatDataFromMongoDB = require("../../db/getChatDataFromMongoDB");
-// // app.use(cors());
-
-// // app.get("/getChat", async (req, res) => {
-// //   const chatData = await getChatDataFromMongoDB();
-// //   res.json(chatData);
-// // });
-
-// // app.listen(3000, () => console.log("Server running on port 3000"));
-
-// // api/getChat.js
-// const getChatDataFromMongoDB = require("../db/getChatDataFromMongoDB");
-
-// module.exports = async (req, res) => {
-//   const chatData = await getChatDataFromMongoDB();
-//   res.json(chatData);
-// };
